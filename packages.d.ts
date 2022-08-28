0ec093d00d2d2b8afcca2fb9e8a0f7045e10e6eb
Jan Thomas <thomas@unionping.com>
2022-08-28 22:55:16 +0200

    Update packages.d.ts

--- packages.d.ts
+++ packages.d.ts
@@ -1,3 +1,14 @@
-interface Packages{
-
+interface Root{
+  "packages":Package[]
 }
+
+interface Package{
+  "repo": string; //Github repo name
+  "owner": string; //Github repo owner
+  "download": string; //File to look at in releases
+  "mods": ModIdent[]; //list of mod identifiers contained in this package
+  "type": PackageType?;
+}
+
+type ModIdent = string; //for later expressional check
+type PackageType = '';
