a54cb0e70d7040007480db75ed4882ad055eea47
Jan Thomas <thomas@unionping.com>
2022-08-28 23:19:08 +0200

    package.d.ts proposal

--- packages.d.ts
+++ packages.d.ts
@@ -1,14 +1,41 @@
-interface Root{
-  "packages":Package[]
-}
+export namespace Packages {
+    export interface Base {
+        "packages": Package[]
+    }
 
-interface Package{
-  "repo": string; //Github repo name
-  "owner": string; //Github repo owner
-  "download": string; //File to look at in releases
-  "mods": ModIdent[]; //list of mod identifiers contained in this package
-  "type": PackageType?;
-}
+    export interface Package {
+        /**
+         * in case of:
+         *  'github_release': the GitHub releases would be the source of the package
+         *  'github_branch': .download would determine what branch source download would be used (this would make .mods obsolete => auto-discover the mods/info)
+         *  'direct': this .download would be a download link
+         * @default 'github_release'
+         */
+        "type"?: PackageType;
 
-type ModIdent = string; //for later expressional check
-type PackageType = '';
+        /**
+         * Package repo name, in case other than github_*: Link to the repo, website or similar
+         */
+        "repo": string;
+
+        /**
+         * The package owner, in case of type github_*: GitHub repo owner
+         */
+        "owner": string;
+
+        /**
+         * where to find the resource
+         */
+        "download": string;
+
+        /**
+         * List of mods to be found in this package
+         */
+        "mods"?: { [id: ModGUID]: ModInfoUrl };
+    }
+
+    export type ModGUID = string; //for later expression check
+    export type PackageType = 'github_release' | 'github_branch' | 'direct';
+    export type VersionNumber = `${bigint}.${bigint}.${bigint}` | `${bigint}.${bigint}` | `${bigint}`;
+    export type ModInfoUrl = string;
+}
